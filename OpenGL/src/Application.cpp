e58f5f7227564fd43f87d1a182ec180604ccdf28
Faruk Hasirci <v-fhasirci@microsoft.com>
2021-10-26 14:31:20 +0300

    Include ImGui lib and demo example.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -17,6 +17,9 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -112,6 +115,15 @@ int main(void)
 
         Renderer renderer;
 
+        // setup ImGui
+        ImGui::CreateContext();
+        ImGui_ImplGlfwGL3_Init(window, true);
+        ImGui::StyleColorsDark();
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
         float r = 0.0f;
         float increment = 0.05f;
         /* Loop until the user closes the window */
@@ -119,6 +131,8 @@ int main(void)
         {
             /* Render here */
             renderer.Clear();
+
+            ImGui_ImplGlfwGL3_NewFrame();
 
             // TODO: Materials should be used 
             shader.Bind();
@@ -133,6 +147,29 @@ int main(void)
 
             r += increment;
 
+            // 1. Show a simple window.
+            // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+                ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+                ImGui::Checkbox("Another Window", &show_another_window);
+
+                if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                    counter++;
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
+
+            ImGui::Render();
+            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -140,7 +177,10 @@ int main(void)
             glfwPollEvents();
         }
     }
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
+    
     return 0;
 }
